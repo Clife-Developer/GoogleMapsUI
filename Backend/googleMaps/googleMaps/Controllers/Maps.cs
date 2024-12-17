fc04c2a654c2a4fa9817b05ba42708656984f7e1
Clife Mhlongo <61283803+Clife-Developer@users.noreply.github.com>
2024-12-17 12:18:18 +0200

    APIs implementation for the backend

---
+++ Backend/googleMaps/googleMaps/Controllers/Maps.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace googleMaps.Controllers
+{
+    [ApiController]
+    [Route("api/getmapslocation")]
+    public class Maps : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult getLocation(double latitude, double longitude)
+        {
+           //no need for this API if its just a simple google maps app, everything can be done in the angular side    
+            return Ok(new { Name = "Location", Lat = latitude, Lng = longitude });
+        }     
+    }
+}
